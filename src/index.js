93c2f8374a5db76f53724b079d7d1f94d9b45f2c
author <jianghequ@jieyuechina.com>
2018-08-01 14:31:14 +0800

    使用webpack require.context 动态加载models

--- src/index.js
+++ src/index.js
@@ -30,7 +30,10 @@ const app = dva({
 
 // 3. Model
 // app.model(require('./models/example').default);
-app.model(require('./models/products').default);
+// app.model(require('./models/producs').default);t
+require('./models').default.forEach(key => {
+  app.model(key.default);
+});
 
 // 4. Router
 app.router(require('./router').default);
