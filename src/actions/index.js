67e341499c3859774e8626b8cbba4111149fda17
author <jianghequ@jieyuechina.com>
2018-08-01 10:48:47 +0800

    加入redux-actions处理actions

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+import { createAction } from 'redux-actions';
+
+export const increment = createAction('products/increment');
+export const decrement = createAction('products/decrement');
+export const asyncDecr = createAction('products/asyncDecr');
--- src/routes/ProductPage.js
+++ src/routes/ProductPage.js
@@ -3,21 +3,26 @@ import { connect } from 'dva';
 import propTypes from 'prop-types';
 import { Button } from 'antd';
 import styles from './ProductPage.css';
+import { increment, asyncDecr } from '../actions';
 
 class ProductPage extends Component {
   constructor() {
     super();
   }
   render() {
-    const { products, dispatch } = this.props;
+    const { products, dispatch, increment, asyncDecr } = this.props;
     return (
       <div className={styles.wrapper}>
         <div className={styles.title}>结果 {products.counter}</div>
         <div>
-          <Button type="primary" onClick={()=>dispatch({type:'products/increment',payload:1})}>incr</Button>
-          <Button type="primary" onClick={()=>dispatch({type:'products/asyncDecr',payload:1})}>incr</Button>
-          {/* <Button type="primary">incr</Button> */}
-          &nbsp;&nbsp;
+          <p className={styles['p-wrapper']}>
+            <Button type="primary" onClick={()=>dispatch({type:'products/increment',payload:1})}>incr</Button>&nbsp;&nbsp;
+            <Button type="primary" onClick={()=>dispatch({type:'products/asyncDecr',payload:1})}>incr</Button>
+          </p>
+          <p className={styles['p-wrapper']}>
+            <Button type="primary" onClick={()=>increment()}>increment</Button>&nbsp;&nbsp;
+            <Button type="primary" onClick={()=>asyncDecr()}>asyncDecr</Button>
+          </p>
           <Button type="primary">decr</Button>
         </div>
       </div>
@@ -35,4 +40,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(ProductPage);
+export default connect(mapStateToProps, { increment, asyncDecr })(ProductPage);
